b082fa7fad3eec903b6a3c2b812d682bda2e2db9
Nick Melms <nmelms92@gmail.com>
2022-10-08 17:27:09 -0400

    styled start button for pressed and unpressed

--- src/components/StartBar.js
+++ src/components/StartBar.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Time from "./Time";
 import windowsIcon from "../assets/windowsIcon.png";
 
 export default function StartBar({ setShowMenu, showMenu }) {
+  const btnRef = useRef();
+  const [pressed, setPressed] = useState(false);
+
+  const handleClick = () => {
+    setPressed(!pressed);
+    setShowMenu(!showMenu);
+    btnRef.current.classList.toggle("pressed");
+  };
+
   return (
     <div className="startBar">
-      <button className="startBtn" onClick={() => setShowMenu(!showMenu)}>
-        <img className="startBtnIcon" src={windowsIcon} />
-        Start
+      <button ref={btnRef} className="startBtn" onClick={handleClick}>
+        <div className="btnContentWrapper">
+          <img className="startBtnIcon" src={windowsIcon} />
+          Start
+        </div>
       </button>
       <div data-testid="clock" className="clock">
         <Time />
