ab8d695b5bb2391dd98ff78a21df3d550688f25c
Nick Melms <nmelms92@gmail.com>
2022-10-09 22:31:33 -0400

    added links to items in start menu

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -5,6 +5,7 @@ import linkedin from "../assets/linkedin.svg";
 import github from "../assets/github.svg";
 import email from "../assets/email.png";
 import folder from "../assets/folder.png";
+import pdf from "../assets/Resume.pdf";
 
 export default function StartMenu() {
   return (
@@ -14,25 +15,33 @@ export default function StartMenu() {
       </div>
 
       <div className="programs">
-        <div className="program">
-          <img alt="folder icon" src={folder} />
-          <u>R</u>esume
-        </div>
-        <div className="program">
-          <img alt="email icon" src={email} />
-          <u>E</u>mail
-        </div>
-        <div className="program">
-          <img alt="github icon" src={github} />
-          <u>G</u>ithub
-        </div>
-        <div
-          style={{ borderBottom: "2px outset lightgray" }}
-          className="program"
-        >
-          <img alt="linkedin icon" src={linkedin} />
-          <u>L</u>inkedin
-        </div>
+        <a href={pdf}>
+          <div className="program">
+            <img alt="folder icon" src={folder} />
+            <u>R</u>esume
+          </div>
+        </a>
+        <a href="mailto: nmelms92@gmail.com">
+          <div className="program">
+            <img alt="email icon" src={email} />
+            <u>E</u>mail
+          </div>
+        </a>
+        <a href="https://github.com/Nmelms">
+          <div className="program">
+            <img alt="github icon" src={github} />
+            <u>G</u>ithub
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/nick-melms-b66300223/">
+          <div
+            style={{ borderBottom: "2px outset lightgray" }}
+            className="program"
+          >
+            <img alt="linkedin icon" src={linkedin} />
+            <u>L</u>inkedin
+          </div>
+        </a>
         <div className="shutdown">
           <img alt="shutdown icon" src={shutdown} />
           Sh<u>u</u>t Down...
