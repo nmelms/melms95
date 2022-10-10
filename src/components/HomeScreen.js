2d93612f3434339ddde421c1ef37e006cedda67b
Nick Melms <nmelms92@gmail.com>
2022-10-09 23:30:52 -0400

    added mycomputer and recycle bin icons

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import StartBar from "./StartBar";
 import StartMenu from "./StartMenu";
+import recycle from "../assets/recycle.png";
+import mycomputer from "../assets/mycomputer.png";
 
 export default function HomeScreen() {
   const [showMenu, setShowMenu] = useState(false);
@@ -9,6 +11,18 @@ export default function HomeScreen() {
     <div className="homeScreen">
       <StartBar showMenu={showMenu} setShowMenu={setShowMenu} />
       {showMenu && <StartMenu />}
+      <div className="desktop">
+        <img
+          alt="bio"
+          style={{ height: "50px", width: "50px" }}
+          src={mycomputer}
+        />
+        <img
+          alt="recycle bin"
+          style={{ height: "50px", width: "50px" }}
+          src={recycle}
+        />
+      </div>
     </div>
   );
 }
--- src/components/HomeScreen.test.js
+++ src/components/HomeScreen.test.js
@@ -8,6 +8,18 @@ test("renders a start menu", async () => {
   expect(startMenu).toBeVisible();
 });
 
+test("renders a mycomputer icon", async () => {
+  render(<HomeScreen />);
+  const computer = screen.getByAltText("bio");
+  expect(computer).toBeVisible();
+});
+
+test("renders a recycle bin icon", async () => {
+  render(<HomeScreen />);
+  const recycle = screen.getByAltText("recycle bin");
+  expect(recycle).toBeVisible();
+});
+
 test("opens Start menu when start button is click", async () => {
   render(<HomeScreen />);
   const startBtn = await screen.findByRole("button", { name: "Start" });
