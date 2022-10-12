a19db1fe8bfe699dc1065c78417d3b3e68128304
Nick Melms <nmelms92@gmail.com>
2022-10-12 00:22:29 -0400

    created a bio window

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,6 +1,13 @@
 import React, { useRef } from "react";
 import myComputer from "../assets/mycomputer.png";
+import pic from "../assets/me.jpg";
 import Draggable from "react-draggable";
+import jsSVG from "../assets/js.svg";
+import reactSVG from "../assets/react.svg";
+import htmlSVG from "../assets/html.svg";
+import cssSVG from "../assets/css.svg";
+import firebaseSVG from "../assets/firebase.svg";
+import jestSVG from "../assets/jest.svg";
 
 export default function Window({ pages, setPages }) {
   const handleCloseClick = () => {
@@ -30,6 +37,33 @@ export default function Window({ pages, setPages }) {
             </button>
           </div>
         </nav>
+        <section className="bioMain">
+          <img className="selfie" src={pic} />
+          <h1>Nick Melms</h1>
+          <h2>Front-end Web Developer </h2>
+
+          <h2 className="bioTitle">
+            <b>About Me</b>
+          </h2>
+          <p>
+            I'm Nick, a front-end web developer. I make responsive web
+            applications that look good and are easy to use. I enjoy keeping up
+            with the latest technologies and use React to build the majority of
+            my sites. When I'm not coding, I enjoy exploring the outdoors.
+          </p>
+          <h2 className="bioTitle">
+            <b>Skills</b>
+          </h2>
+          <div className="logos">
+            <img alt="react " className="techLogo" src={reactSVG} />
+            <img alt="javascript " className="techLogo" src={jsSVG} />
+            <img alt="jest " className="techLogo" src={jestSVG} />
+            <img alt="firebase " className="techLogo" src={firebaseSVG} />
+            <img alt="html " className="techLogo" src={htmlSVG} />
+            <img alt="css " className="techLogo" src={cssSVG} />
+          </div>
+          <p>React/Javascript/Jest/fireBase/HTML/CSS</p>
+        </section>
       </div>
     </Draggable>
   );
