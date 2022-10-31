72729b2fe06486f426ba7972f7cdd386e0857566
Nick Melms <nmelms92@gmail.com>
2022-10-30 23:45:25 -0400

    added logic show neighboring bombs

--- src/components/minesweeper/MineSweeper.js
+++ src/components/minesweeper/MineSweeper.js
@@ -7,7 +7,7 @@ export default function MineSweeper() {
   const row = 10;
   const col = 10;
 
-  const numOfBombs = 20;
+  const numOfBombs = 40;
   const gameBoard = [];
 
   for (let i = 0; i < row; i++) {
@@ -19,11 +19,12 @@ export default function MineSweeper() {
   const handleTileClick = (tile) => {
     console.log("x: " + tile.x);
     console.log("y: " + tile.y);
+    console.log("value: " + tile.value);
   };
 
   const placeBombs = () => {
     const bombsArr = [];
-    for (let i = 0; i < numOfBombs; i++) {
+    while (bombsArr.length < numOfBombs) {
       const num = Math.floor(Math.random() * (99 - 0 + 1) + 0);
       if (!bombsArr.includes(num)) {
         bombsArr.push(num);
@@ -34,7 +35,71 @@ export default function MineSweeper() {
     }
   };
 
+  const setNumbers = () => {
+    console.log(gameBoard[1]);
+    gameBoard.map((tile, index) => {
+      let bombCount = 0;
+      //check above all tiles not on top row
+      if (tile.x !== 0 && gameBoard[index - 10].value === "x") {
+        if (tile.value !== "x") {
+          bombCount++;
+          tile.value = bombCount;
+        }
+      }
+      //check top right on all tiles not on top row
+      if (tile.x !== 0 && gameBoard[index - 9].value === "x") {
+        if (tile.value !== "x") {
+          bombCount++;
+          tile.value = bombCount;
+        }
+      }
+      // check to right of all tiles not on right side
+      if (tile.y !== 9 && gameBoard[index + 1].value === "x") {
+        if (tile.value !== "x") {
+          bombCount++;
+          tile.value = bombCount;
+        }
+      }
+      //check bottom right of all tiles not on right side
+      if (tile.x !== 9 && tile.y !== 9) {
+        if (gameBoard[index + 11].value === "x" && tile.value !== "x") {
+          bombCount++;
+          tile.value = bombCount;
+        }
+      }
+      //check below every tile not on bottom row
+      if (tile.x !== 9 && gameBoard[index + 10].value === "x") {
+        if (tile.value !== "x") {
+          bombCount++;
+          tile.value = bombCount;
+        }
+      }
+      // check bottom left of every tile not on bottom
+      if (tile.y !== 0 && tile.x !== 9) {
+        if (tile.value !== "x" && gameBoard[index + 9].value === "x") {
+          bombCount++;
+          tile.value = bombCount;
+        }
+      }
+      //check left of every tile not on left side
+      if (tile.y !== 0 && gameBoard[index - 1].value === "x") {
+        if (tile.value !== "x") {
+          bombCount++;
+          tile.value = bombCount;
+        }
+      }
+      //check top left of every title
+      if (tile.y !== 0 && tile.x !== 0) {
+        if (tile.value !== "x" && gameBoard[index - 11].value === "x") {
+          bombCount++;
+          tile.value = bombCount;
+        }
+      }
+    });
+  };
+
   placeBombs();
+  setNumbers();
 
   console.log(gameBoard);
 
