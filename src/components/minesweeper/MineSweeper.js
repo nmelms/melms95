e64e45ef574d738b4bc665b0dcdc897cfa6d9780
Nick Melms <nmelms92@gmail.com>
2022-11-02 01:06:03 -0400

    created an initBoard function and saved board into state so i can keep track of how many flags are on the board

--- src/components/minesweeper/MineSweeper.js
+++ src/components/minesweeper/MineSweeper.js
@@ -1,10 +1,13 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Tile from "./Tile";
 import Bomb from "./Bomb";
 import { getByTitle } from "@testing-library/dom";
+import ScoreBoard from "./ScoreBoard";
 
 export default function MineSweeper() {
   const [newGame, setNewGame] = useState(false);
+  const [gameBoard, setGameBoard] = useState([]);
+  const [numOfFlags, setNumOfFlags] = useState(0);
   const tileRef = useRef([]);
   const gameOverRef = useRef();
   let gameOver = false;
@@ -12,26 +15,84 @@ export default function MineSweeper() {
   const col = 10;
 
   const numOfBombs = 10;
-  let gameBoard = [];
 
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < col; j++) {
-      gameBoard.push({ value: "", x: i, y: j, hidden: true, flag: false });
+  const initBoard = () => {
+    let newGameBoard = [];
+    let bombsArr = [];
+    const setNumbers = () => {
+      newGameBoard.map((tile, index) => {
+        let bombCount = 0;
+        //check above all tiles not on top row
+        if (tile.x !== 0 && newGameBoard[index - 10].value === "x") {
+          if (tile.value !== "x") {
+            bombCount++;
+            tile.value = bombCount;
+          }
+        }
+        //check top right on all tiles not on top row
+        if (
+          tile.x !== 0 &&
+          tile.y !== 9 &&
+          newGameBoard[index - 9].value === "x"
+        ) {
+          if (tile.value !== "x") {
+            bombCount++;
+            tile.value = bombCount;
+          }
+        }
+        // check to right of all tiles not on right side
+        if (tile.y !== 9 && newGameBoard[index + 1].value === "x") {
+          if (tile.value !== "x") {
+            bombCount++;
+            tile.value = bombCount;
+          }
+        }
+        //check bottom right of all tiles not on right side
+        if (tile.x !== 9 && tile.y !== 9) {
+          if (newGameBoard[index + 11].value === "x" && tile.value !== "x") {
+            bombCount++;
+            tile.value = bombCount;
+          }
+        }
+        //check below every tile not on bottom row
+        if (tile.x !== 9 && newGameBoard[index + 10].value === "x") {
+          if (tile.value !== "x") {
+            bombCount++;
+            tile.value = bombCount;
+          }
+        }
+        // check bottom left of every tile not on bottom
+        if (tile.y !== 0 && tile.x !== 9) {
+          if (tile.value !== "x" && newGameBoard[index + 9].value === "x") {
+            bombCount++;
+            tile.value = bombCount;
+          }
+        }
+        //check left of every tile not on left side
+        if (tile.y !== 0 && newGameBoard[index - 1].value === "x") {
+          if (tile.value !== "x") {
+            bombCount++;
+            tile.value = bombCount;
+          }
+        }
+        //check top left of every title
+        if (tile.y !== 0 && tile.x !== 0) {
+          if (tile.value !== "x" && newGameBoard[index - 11].value === "x") {
+            bombCount++;
+            tile.value = bombCount;
+          }
+        }
+      });
+    };
+
+    //creates a new board
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < col; j++) {
+        newGameBoard.push({ value: "", x: i, y: j, hidden: true, flag: false });
+      }
     }
-  }
-  const resetClick = () => {
-    setNewGame(!newGame);
-    gameBoard.map((tile, i) => {
-      tileRef.current[i].classList.remove("visible");
-      tileRef.current[i].classList.remove("flag");
-    });
-    gameOverRef.current.style.display = "none";
-    placeBombs();
-    setNumbers();
-  };
 
-  const placeBombs = () => {
-    const bombsArr = [];
+    //add mines
     while (bombsArr.length < numOfBombs) {
       const num = Math.floor(Math.random() * (99 - 0 + 1) + 0);
       if (!bombsArr.includes(num)) {
@@ -39,8 +100,20 @@ export default function MineSweeper() {
       }
     }
     for (let i = 0; i < bombsArr.length; i++) {
-      gameBoard[bombsArr[i]].value = "x";
+      newGameBoard[bombsArr[i]].value = "x";
     }
+    //setnumber of mines on tile
+    setNumbers();
+    setGameBoard(newGameBoard);
+  };
+
+  const resetClick = () => {
+    gameBoard.map((tile, i) => {
+      tileRef.current[i].classList.remove("visible");
+      tileRef.current[i].classList.remove("flag");
+    });
+    gameOverRef.current.style.display = "none";
+    initBoard();
   };
 
   const handleTileClick = (tile, i) => {
@@ -49,7 +122,6 @@ export default function MineSweeper() {
     tile.hidden = false;
     tile.value === "x" && bombClick();
     tile.value === "" && check(tile, i);
-    tile.value !== "" && tile.value !== "x" && console.log("hello");
   };
 
   const bombClick = () => {
@@ -58,32 +130,32 @@ export default function MineSweeper() {
 
   const handleRightClick = (e, tile, i) => {
     e.preventDefault();
-    console.log(tile);
     if (tile.hidden === false) return;
     tile.flag = !tile.flag;
     tile.flag === true
       ? tileRef.current[i].classList.add("flag")
       : tileRef.current[i].classList.remove("flag");
+
+    tile.flag === true
+      ? setNumOfFlags(numOfFlags + 1)
+      : setNumOfFlags(numOfFlags - 1);
   };
 
   const check = (tile, i) => {
     // //check top
     if (tile.x !== 0 && gameBoard[i - 10].hidden === true) {
-      console.log("check");
       tileRef.current[i - 10].classList.add("visible");
       tile.hidden = false;
       handleTileClick(gameBoard[i - 10], i - 10);
     }
     //check top right
     if (tile.y !== 9 && tile.x !== 0 && gameBoard[i - 9].hidden === true) {
-      console.log(gameBoard[i - 9]);
       tileRef.current[i - 9].classList.add("visible");
       tile.hidden = false;
       handleTileClick(gameBoard[i - 9], i - 9);
     }
     // check right
     if (tile.y !== 9 && gameBoard[i + 1].hidden === true) {
-      console.log(gameBoard[i + 1]);
       tileRef.current[i + 1].classList.add("visible");
       tile.hidden = false;
       handleTileClick(gameBoard[i + 1], i + 1);
@@ -91,7 +163,6 @@ export default function MineSweeper() {
     //check bottom right
     if (tile.x !== 9 && tile.y !== 9 && gameBoard[i + 11].hidden === true) {
       tileRef.current[i + 11].classList.add("visible");
-      console.log(tile.x, tile.y);
       tile.hidden = false;
       handleTileClick(gameBoard[i + 11], i + 11);
     }
@@ -121,74 +192,13 @@ export default function MineSweeper() {
     }
   };
 
-  const setNumbers = () => {
-    console.log(gameBoard[1]);
-    gameBoard.map((tile, index) => {
-      let bombCount = 0;
-      //check above all tiles not on top row
-      if (tile.x !== 0 && gameBoard[index - 10].value === "x") {
-        if (tile.value !== "x") {
-          bombCount++;
-          tile.value = bombCount;
-        }
-      }
-      //check top right on all tiles not on top row
-      if (tile.x !== 0 && gameBoard[index - 9].value === "x") {
-        if (tile.value !== "x") {
-          bombCount++;
-          tile.value = bombCount;
-        }
-      }
-      // check to right of all tiles not on right side
-      if (tile.y !== 9 && gameBoard[index + 1].value === "x") {
-        if (tile.value !== "x") {
-          bombCount++;
-          tile.value = bombCount;
-        }
-      }
-      //check bottom right of all tiles not on right side
-      if (tile.x !== 9 && tile.y !== 9) {
-        if (gameBoard[index + 11].value === "x" && tile.value !== "x") {
-          bombCount++;
-          tile.value = bombCount;
-        }
-      }
-      //check below every tile not on bottom row
-      if (tile.x !== 9 && gameBoard[index + 10].value === "x") {
-        if (tile.value !== "x") {
-          bombCount++;
-          tile.value = bombCount;
-        }
-      }
-      // check bottom left of every tile not on bottom
-      if (tile.y !== 0 && tile.x !== 9) {
-        if (tile.value !== "x" && gameBoard[index + 9].value === "x") {
-          bombCount++;
-          tile.value = bombCount;
-        }
-      }
-      //check left of every tile not on left side
-      if (tile.y !== 0 && gameBoard[index - 1].value === "x") {
-        if (tile.value !== "x") {
-          bombCount++;
-          tile.value = bombCount;
-        }
-      }
-      //check top left of every title
-      if (tile.y !== 0 && tile.x !== 0) {
-        if (tile.value !== "x" && gameBoard[index - 11].value === "x") {
-          bombCount++;
-          tile.value = bombCount;
-        }
-      }
-    });
-  };
-
-  placeBombs();
-  setNumbers();
+  useEffect(() => {
+    initBoard();
+  }, []);
 
   return (
     <>
+      <ScoreBoard numOfBombs={numOfBombs} numOfFlags={numOfFlags} />
       <div className="gameBoard">
         <div ref={gameOverRef} style={{ display: "none" }} className="gameover">
           <h1>GameOver</h1>
@@ -201,7 +211,6 @@ export default function MineSweeper() {
               onContextMenu={(e) => handleRightClick(e, tile, i)}
               onClick={() => handleTileClick(tile, i)}
               className="tile"
-              // style={{ color: tile.hidden ? "black" : "white" }}
             >
               {tile.value}
             </div>
